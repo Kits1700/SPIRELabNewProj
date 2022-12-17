e30fb84e814c77ca20680e7eb73ab6600f2d9998
kits1700 <saikeerthana.arun@gmail.com>
2022-12-17 23:22:09 +0530

    added changes

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -7,66 +7,7 @@ import { Link } from 'react-router-dom';
 import Footer from '../Footer';
 export default function Services() {
   return (<>
-  <div className='cards'>
-      <h2> Project Investigator</h2>
-      <br>
-      </br>
-      <div className='cards__container'>
-        <div className='cards__wrapper'>
-          <ul className='cards__items'>
-            <CardItem class = "one"
-              src='images/PKG1.jpeg'
-              text='Prof. Prasanta Kumar Ghosh'
-             
-              path='/services'
-            />
-            {/* <CardItem
-              src='images/brown.jpg'
-              text=''
-            
-              path='/services'
-            />
-             <CardItem
-              src='images/brown.jpg'
-              text=''
-             
-              path='/services'
-            /> */}
-            
-          </ul>
-          <h2> Partners</h2>
-         <br>
-         </br>
-          <a href='https://ee.iisc.ac.in/~prasantg/'>
-          <CardItem class = "one"
-              src='images/artpark.png'
-              text = 'ARTPARK'
-           
-            />
-            </a>
-          {/* <ul className='cards__items'>
-            <CardItem
-              src='images/brown.jpg'
-              text='Lorem Ipsum Lorem Ipsum Lorem Ipsum'
-              
-              path='/services'
-            />
-            <CardItem
-              src='images/brown.jpg'
-              text='Lorem Ipsum Lorem Ipsum Lorem Ipsum'
-           
-              path='/products'
-            />
-            <CardItem
-              src='images/brown.jpg'
-              text=''
-              
-              path='/sign-up'
-            />
-          </ul> */}
-        </div>
-      </div>
-    </div>
+ 
   <Footer />
   </>);
 }
