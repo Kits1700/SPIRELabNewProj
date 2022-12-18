73305211b033523b2187c563e4c93b246e167ef7
kits1700 <saikeerthana.arun@gmail.com>
2022-12-18 15:08:24 +0530

    added new changes

--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -30,29 +30,407 @@ export default function Products() {
   <input type="radio" class="tabs__radio" name="tabs-example" id="tab1" />
   <label for="tab1" class="tabs__label">Data Summary</label>
   <div class="tabs__content">
-  <h2>Karnataka</h2>
+<br></br>
+<img src = "/images/bihar.gif" width="250" height ="250"></img>
+<br></br>
+ <h2>Bihar</h2>
+  <br>
+  </br>
   <table>
   <tr>
     <th>District</th>
     <th>Duration(hrs)</th>
     <th>No. of Speakers</th>
   </tr>
-  <tr>
-  
+  <tr >
+  <td>Araria</td>
+  </tr>
+  <tr >
+   <td>Begusarai</td>
   </tr>
   <tr>
-   
+    <td>Darbhanga</td>
   </tr>
   <tr>
-    
+  <td>Gopalganj </td>
   </tr>
   <tr>
-  
+<td>Jahanabad</td>
   </tr>
-</table>
+  <tr>
+    <td>Jamui</td>
+  </tr>
+  <tr>
+    <td>
+    Sitamarhi
+    </td>
+  </tr>
+</table> 
 
+<br>
+</br>
+<img src = "/images/maha.gif" width="250" height ="250"></img>
+<br>
+</br>
+<h2>Maharashtra</h2>
+<table>
+  <tr>
+    <th>District</th>
+    <th>Duration(hrs)</th>
+    <th>No. of Speakers</th>
+  </tr>
+  <tr>
+  <td>Araria</td>
+  </tr>
+  <tr>
+   <td>Begusarai</td>
+  </tr>
+  <tr>
+    <td>Darbhanga</td>
+  </tr>
+  <tr>
+  <td>Gopalganj </td>
+  </tr>
+  <tr>
+<td>Jahanabad</td>
+  </tr>
+  <tr>
+    <td>Jamui</td>
+  </tr>
+  <tr>
+    <td>
+    Sitamarhi
+    </td>
+  </tr>
+</table> 
+<br>
+</br>
+<img src = "/images/chhattisgarh.gif" width="250" height ="250"></img>
+<br>
+</br>
+<h2>Chhattisgarh</h2>
+<table>
+  <tr>
+    <th>District</th>
+    <th>Duration(hrs)</th>
+    <th>No. of Speakers</th>
+  </tr>
+  <tr>
+  <td>Araria</td>
+  </tr>
+  <tr>
+   <td>Begusarai</td>
+  </tr>
+  <tr>
+    <td>Darbhanga</td>
+  </tr>
+  <tr>
+  <td>Gopalganj </td>
+  </tr>
+  <tr>
+<td>Jahanabad</td>
+  </tr>
+  <tr>
+    <td>Jamui</td>
+  </tr>
+  <tr>
+    <td>
+    Sitamarhi
+    </td>
+  </tr>
+</table> 
+<br>
+</br>
+<img src = "/images/karnataka.gif" width="250" height ="250"></img>
+<br></br>
+<h2>Karnataka</h2>
+<table>
+  <tr>
+    <th>District</th>
+    <th>Duration(hrs)</th>
+    <th>No. of Speakers</th>
+  </tr>
+  <tr>
+  <td>Araria</td>
+  </tr>
+  <tr>
+   <td>Begusarai</td>
+  </tr>
+  <tr>
+    <td>Darbhanga</td>
+  </tr>
+  <tr>
+  <td>Gopalganj </td>
+  </tr>
+  <tr>
+<td>Jahanabad</td>
+  </tr>
+  <tr>
+    <td>Jamui</td>
+  </tr>
+  <tr>
+    <td>
+    Sitamarhi
+    </td>
+  </tr>
+</table> 
+<br>
+</br>
+<img src = "/images/uttar05s.gif" width="250" height ="250"></img>
+<br></br>
+<h2>Uttar Pradesh</h2>
+<table>
+  <tr>
+    <th>District</th>
+    <th>Duration(hrs)</th>
+    <th>No. of Speakers</th>
+  </tr>
+  <tr>
+  <td>Araria</td>
+  </tr>
+  <tr>
+   <td>Begusarai</td>
+  </tr>
+  <tr>
+    <td>Darbhanga</td>
+  </tr>
+  <tr>
+  <td>Gopalganj </td>
+  </tr>
+  <tr>
+<td>Jahanabad</td>
+  </tr>
+  <tr>
+    <td>Jamui</td>
+  </tr>
+  <tr>
+    <td>
+    Sitamarhi
+    </td>
+  </tr>
+</table> 
+<br>
+</br>
+<img src = "/images/andhra09s.gif" width="250" height ="250"></img>
+<br></br>
+<h2>Andhra Pradesh</h2>
+<table>
+  <tr>
+    <th>District</th>
+    <th>Duration(hrs)</th>
+    <th>No. of Speakers</th>
+  </tr>
+  <tr>
+  <td>Araria</td>
+  </tr>
+  <tr>
+   <td>Begusarai</td>
+  </tr>
+  <tr>
+    <td>Darbhanga</td>
+  </tr>
+  <tr>
+  <td>Gopalganj </td>
+  </tr>
+  <tr>
+<td>Jahanabad</td>
+  </tr>
+  <tr>
+    <td>Jamui</td>
+  </tr>
+  <tr>
+    <td>
+    Sitamarhi
+    </td>
+  </tr>
+</table> 
+<br>
+</br>
+<img src = "/images/uttar05s.gif" width="250" height ="250"></img>
+<br></br>
+<h2>Rajasthan</h2>
+<table>
+  <tr>
+    <th>District</th>
+    <th>Duration(hrs)</th>
+    <th>No. of Speakers</th>
+  </tr>
+  <tr>
+  <td>Araria</td>
+  </tr>
+  <tr>
+   <td>Begusarai</td>
+  </tr>
+  <tr>
+    <td>Darbhanga</td>
+  </tr>
+  <tr>
+  <td>Gopalganj </td>
+  </tr>
+  <tr>
+<td>Jahanabad</td>
+  </tr>
+  <tr>
+    <td>Jamui</td>
+  </tr>
+  <tr>
+    <td>
+    Sitamarhi
+    </td>
+  </tr>
+</table> 
+<br>
+</br>
+
+<img src = "/images/bengale09s.gif" width="250" height ="250"></img>
+<br></br>
+<h2>West Bengal</h2>
+<table>
+  <tr>
+    <th>District</th>
+    <th>Duration(hrs)</th>
+    <th>No. of Speakers</th>
+  </tr>
+  <tr>
+  <td>Araria</td>
+  </tr>
+  <tr>
+   <td>Begusarai</td>
+  </tr>
+  <tr>
+    <td>Darbhanga</td>
+  </tr>
+  <tr>
+  <td>Gopalganj </td>
+  </tr>
+  <tr>
+<td>Jahanabad</td>
+  </tr>
+  <tr>
+    <td>Jamui</td>
+  </tr>
+  <tr>
+    <td>
+    Sitamarhi
+    </td>
+  </tr>
+</table> 
+<br>
+</br>
+<img src = "/images/telangana.gif" width="250" height ="250"></img>
+<br>
+</br>
+
+<h2>Telangana</h2>
+<table class = "last">
+  <tr>
+    <th>District</th>
+    <th>Duration(hrs)</th>
+    <th>No. of Speakers</th>
+  </tr>
+  <tr>
+  <td>Araria</td>
+  </tr>
+  <tr>
+   <td>Begusarai</td>
+  </tr>
+  <tr>
+    <td>Darbhanga</td>
+  </tr>
+  <tr>
+  <td>Gopalganj </td>
+  </tr>
+  <tr>
+<td>Jahanabad</td>
+  </tr>
+  <tr>
+    <td>Jamui</td>
+  </tr>
+  <tr>
+    <td>
+    Sitamarhi
+    </td>
+  </tr>
+</table> 
+
+<br>
+</br>
+<img src = "/images/goa09s.gif" width="250" height ="250"></img>
+<br>
+</br>
+
+<h2>Goa</h2>
+<table class = "last">
+  <tr>
+    <th>District</th>
+    <th>Duration(hrs)</th>
+    <th>No. of Speakers</th>
+  </tr>
+  <tr>
+  <td>Araria</td>
+  </tr>
+  <tr>
+   <td>Begusarai</td>
+  </tr>
+  <tr>
+    <td>Darbhanga</td>
+  </tr>
+  <tr>
+  <td>Gopalganj </td>
+  </tr>
+  <tr>
+<td>Jahanabad</td>
+  </tr>
+  <tr>
+    <td>Jamui</td>
+  </tr>
+  <tr>
+    <td>
+    Sitamarhi
+    </td>
+  </tr>
+</table> 
+<br></br>
+<img src = "/images/uttarakhand05s.gif" width="250" height ="250"></img>
+<br></br>
+<h2>Uttarakhand</h2>
+<table class = "last">
+  <tr>
+    <th>District</th>
+    <th>Duration(hrs)</th>
+    <th>No. of Speakers</th>
+  </tr>
+  <tr>
+  <td>Araria</td>
+  </tr>
+  <tr>
+   <td>Begusarai</td>
+  </tr>
+  <tr>
+    <td>Darbhanga</td>
+  </tr>
+  <tr>
+  <td>Gopalganj </td>
+  </tr>
+  <tr>
+<td>Jahanabad</td>
+  </tr>
+  <tr>
+    <td>Jamui</td>
+  </tr>
+  <tr>
+    <td>
+    Sitamarhi
+    </td>
+  </tr>
+</table> 
+<br></br>
+
+<br></br>
   <Footer />
-   
+  <br></br>
+
+<br></br>
   </div>
   <input type="radio" class="tabs__radio" name="tabs-example" id="tab2" checked/>
   <label for="tab2" class="tabs__label">Data Download</label>
