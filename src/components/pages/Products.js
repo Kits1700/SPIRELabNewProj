93c945fbf793e197031e88a3b35cb3a80d7ff09a
kits1700 <saikeerthana.arun@gmail.com>
2022-12-18 13:02:01 +0530

    added new changes

--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -38,24 +38,16 @@ export default function Products() {
     <th>No. of Speakers</th>
   </tr>
   <tr>
-    <td>Peter</td>
-    <td>Griffin</td>
-    <td>$100</td>
+  
   </tr>
   <tr>
-    <td>Lois</td>
-    <td>Griffin</td>
-    <td>$150</td>
+   
   </tr>
   <tr>
-    <td>Joe</td>
-    <td>Swanson</td>
-    <td>$300</td>
+    
   </tr>
   <tr>
-    <td>Cleveland</td>
-    <td>Brown</td>
-    <td>$250</td>
+  
   </tr>
 </table>
 
