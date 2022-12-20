6d11a1b1a599bf183260837770034624f151b6a2
kits1700 <saikeerthana.arun@gmail.com>
2022-12-20 09:21:06 +0530

    added changes

--- src/components/pages/Media.js
+++ src/components/pages/Media.js
@@ -35,7 +35,8 @@ export default function Media()
       <br>
       </br>
          <h2>Articles</h2>
-      <a class = "article" href='https://www.livemint.com/technology/tech-news/artparkiisc-google-to-bring-innovation-to-india-s-diverse-languages-11671445526332.html'><p><u>Artpark-IISc, Google to bring innovation to India’s diverse languages</u></p></a>
+      <a class = "article" href='https://www.livemint.com/technology/tech-news/artparkiisc-google-to-bring-innovation-to-india-s-diverse-languages-11671445526332.html'><p><u>- Artpark-IISc, Google to bring innovation to India’s diverse languages</u></p></a>
+      <a class = "article" href='https://www.livemint.com/companies/start-ups/google-taps-ai-to-grasp-india-s-language-diversity-11671466688191.html?utm_source=pocket_mylist'><p><u>- Google taps AI to grasp India’s language diversity</u></p></a>
 
     <br></br>
  
